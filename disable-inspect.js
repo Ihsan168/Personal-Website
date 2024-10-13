bf23f849eecdb25f2eda35bce54954fb06e01ee0
FAQIH MUHAMMAD IHSAN <134912000+Ihsan168@users.noreply.github.com>
2024-10-13 12:20:18 +0700

    Add files via upload

---
+++ disable-inspect.js
@@ -0,0 +1,16 @@
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
